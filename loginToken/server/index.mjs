b1eab2560c27cd535d494a5a7e465eb06c3fbf30
Joe <joeisgood0819@gmail.com>
2024-01-16 16:27:34 +0800

    0116

--- loginToken/server/index.mjs
+++ loginToken/server/index.mjs
@@ -9,8 +9,6 @@ const upload = multer();
 dotenv.config();
 const secretKey = process.env.SECRET_KEY;
 
-console.log(secretKey);
-
 const wwhitelist = [
   "http://localhost:5500",
   "http://localhost:3000",
@@ -37,12 +35,58 @@ app.get("/", (req, res) => {
   res.send("首頁");
 });
 
-app.post("/api/users/login", (req, res) => {
-  res.status(200).json({ message: "login success" });
+app.post("/api/users/login", upload.none(), (req, res) => {
+  const { account, password } = req.body;
+  const user = users.find(
+    (u) => u.account === account && u.password === password
+  );
+  if (user) {
+    const token = jwt.sign(
+      {
+        account: user.account,
+        name: user.name,
+        mail: user.mail,
+        head: user.head,
+      },
+      secretKey,
+      { expiresIn: "30m" }
+    );
+    res.status(200).json({
+      status: "success",
+      token,
+    });
+  } else {
+    res.status(400).json({
+      status: "error",
+      message: "帳號或密碼錯誤",
+    });
+  }
 });
 
-app.post("/api/users/logout", (req, res) => {
-  res.status(200).json({ message: "logout success" });
+app.post("/api/users/logout", checkToken, (req, res) => {
+  console.log(req.decoded);
+  const user = users.find((u) => u.account === req.decoded.account);
+  if (user) {
+    const token = jwt.sign(
+      {
+        account: " ",
+        name: " ",
+        mail: " ",
+        head: " ",
+      },
+      secretKey,
+      { expiresIn: "-10s" }
+    );
+    res.status(200).json({
+      status: "success",
+      token,
+    });
+  } else {
+    res.status(401).json({
+      status: "error",
+      message: "登出失敗",
+    });
+  }
 });
 
 app.post("/api/users/status", (req, res) => {
@@ -52,3 +96,27 @@ app.post("/api/users/status", (req, res) => {
 app.listen(3000, () => {
   console.log("server at http://localhost:3000");
 });
+
+function checkToken(req, res, next) {
+  // console.log(req.get("authorization"));
+  let token = req.get("authorization");
+  if (token) {
+    jwt.verify(token, secretKey, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({
+          status: "error",
+          message: "登入認證失效，請重新登入",
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+    next();
+  } else {
+    return res.status(401).json({
+      status: "error",
+      message: "無登入認證資料，請重新登入",
+    });
+  }
+}
